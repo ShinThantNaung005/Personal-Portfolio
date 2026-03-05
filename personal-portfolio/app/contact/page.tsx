16f98cad08d30bf095a9783c21c55068c2e27a27
ShinThantNaung <shinthantnaung112@gmail.com>
2026-03-05 20:09:04 +0630

    added experience page contents

--- personal-portfolio/app/contact/page.tsx
+++ personal-portfolio/app/contact/page.tsx
@@ -1,3 +1,4 @@
+
 export default function ContactPage() {
     return (
         <div className="relative z-10 min-h-screen">
@@ -9,12 +10,12 @@ export default function ContactPage() {
                 <div className="flex-1 bg-white/30 backdrop-blur-md rounded-2xl border border-white/20 shadow-lg p-6">
                     <form action="mailto:shinthantnaung112@gmail.com" method="post" encType="text/plain" className="flex flex-col gap-4">
                         <div>
-                            <label htmlFor="fullName" className="font-semibold block mb-2 text-gray-900 dark:text-white transition-colors duration-500">Full Name</label>
-                            <input placeholder="Enter your full name" type="text" id="fullName" name="fullName" className="pl-5 border border-gray-400 dark:border-gray-600 p-2 rounded-2xl w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-white transition-colors duration-500" required />
-                            <label htmlFor="email" className="font-semibold block mb-2 mt-4 text-gray-900 dark:text-white transition-colors duration-500">Email</label>
-                            <input placeholder="Enter your email" type="email" id="email" name="email" className="pl-5 border border-gray-400 dark:border-gray-600 p-2 rounded-2xl w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-white transition-colors duration-500" required />
-                            <label htmlFor="message" className="font-semibold block mb-2 mt-4 text-gray-900 dark:text-white transition-colors duration-500">Message</label>
-                            <textarea placeholder="Enter your message" id="message" name="message" rows={5} className="pl-5 border border-gray-400 dark:border-gray-600 p-2 rounded-2xl w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-white transition-colors duration-500" required></textarea>
+                            <label htmlFor="fullName" className="font-semibold block mb-2 text-gray-900 dark:text-white">Full Name</label>
+                            <input placeholder = "Enter your full name" type="text" id="fullName" name="fullName" className="pl-5 border border-gray-400 dark:border-white-600 p-2 rounded-2xl w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-white" required/>
+                            <label htmlFor="email" className="font-semibold block mb-2 mt-4 text-gray-900 dark:text-white">Email</label>
+                            <input placeholder = "Enter your email" type="email" id="email" name="email" className="pl-5 border border-gray-400 dark:border-white-600 p-2 rounded-2xl w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-white" required/>
+                            <label htmlFor="message" className="font-semibold block mb-2 mt-4 text-gray-900 dark:text-white">Message</label>
+                            <textarea placeholder = "Enter your message" id="message" name="message" rows={5} className="pl-5 border border-gray-400 dark:border-white-600 p-2 rounded-2xl w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-white" required></textarea>
                         </div>
                         <button type="submit" className="bg-blue-600 text-white p-2 rounded-2xl hover:bg-blue-700">Send</button>
                     </form>
--- personal-portfolio/app/experience/page.tsx
+++ personal-portfolio/app/experience/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
+import capoo from '../../public/capoo-crying.webp'
 
 const ExperiencePage: React.FC = () => {
     return (
-        <p>
-            This is the Experience page. Here you can list your work experience, projects, or any relevant information about your professional background.
-        </p>
+        <div>
+            <Image src={capoo} alt="Capoo Crying" width={400} height={400} className="mx-auto mt-20" />
+            <h1 className="text-4xl font-bold text-center mt-10">I still don't have experiences to showcase :(</h1>
+        </div>
     )
 }
 
